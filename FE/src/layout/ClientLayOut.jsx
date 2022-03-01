11de9b95fdb391fc6d5b38492801ad5f7f3c3bf7
Maikol2000 <cuonglw27@gmail.com>
2022-03-01 14:47:36 +0700

    j do

--- FE/src/layout/ClientLayOut.jsx
+++ FE/src/layout/ClientLayOut.jsx
@@ -1,14 +1,14 @@
-import Footer from "component/Footer/Footer";
-import Header from "component/Header/Header";
+// import Footer from "component/Footer/Footer";
+// import Header from "component/Header/Header";
 import React from "react";
 import { Outlet } from "react-router-dom";
 
 export default function ClientLayOut() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <Outlet />
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
